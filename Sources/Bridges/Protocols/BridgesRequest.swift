b98e7eb7ef495997d14996fbb0764a2a14d8e9b6
Mihael Isaev <isaev.mihael@gmail.com>
2020-01-30 06:09:28 +0400

    Implement `BridgesRequest` protocol

---
+++ Sources/Bridges/Protocols/BridgesRequest.swift
@@ -0,0 +1,10 @@
+//
+//  BridgesRequest.swift
+//  Bridges
+//
+//  Created by Mihael Isaev on 30.01.2020.
+//
+
+public protocol BridgesRequest {
+    var bridgesApplication: BridgesApplication { get }
+}
