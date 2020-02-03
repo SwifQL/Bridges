b1b8076eac7b0dda34cf8ab93377931f20e692b4
Mihael Isaev <isaev.mihael@gmail.com>
2020-02-03 21:13:33 +0400

    TableUpdate: implement partial update logic

--- Sources/Bridges/Helpers/TableUpdate.swift
+++ Sources/Bridges/Helpers/TableUpdate.swift
@@ -10,23 +10,22 @@ import SwifQL
 
 extension Table {
     public func update<Column>(on keyColumn: KeyPath<Self, Column>, on conn: BridgeConnection) -> EventLoopFuture<Self> where Column: ColumnRepresentable {
-        var items: [(String, SwifQLable)] = columns.compactMap {
+        var items: [(String, SwifQLable, Bool)] = columns.compactMap {
             guard let value = $0.1.inputValue as? SwifQLable else { return nil }
-            return ($0.0, value)
+            return ($0.0, value, $0.1.isChanged)
         }
         let keyColumnName = Self.key(for: keyColumn)
         let keyColumn = Path.Column(keyColumnName)
         guard let keyColumnValue = items.first(where: { $0.0 == keyColumnName })?.1 else {
             return conn.eventLoop.makeFailedFuture(BridgesError.valueIsNilInKeyColumnUpdateIsImpossible)
         }
-        items = items.filter { $0.0 != keyColumnName }
+        items = items.filter { $0.0 != keyColumnName && $0.2 }
         let query = SwifQL
             .update(Self.table)
             .set[items: items.map { Path.Column($0.0) == $0.1 }]
             .where(keyColumn == keyColumnValue)
             .returning
             .asterisk
-        print(query.prepare(conn.dialect).plain)
         return conn.query(sql: query, decoding: Self.self).flatMapThrowing { rows in
             guard let row = rows.first else { throw BridgesError.failedToDecodeWithReturning }
             return row
