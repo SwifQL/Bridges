eedb9f5dcb5b0af898e0fa2878b502a5c1aa6d24
nerzh <emptystamp@gmail.com>
2023-04-04 09:19:10 +0200

    fix

--- Sources/Bridges/Protocols/AnyMigration.swift
+++ Sources/Bridges/Protocols/AnyMigration.swift
@@ -11,8 +11,8 @@ public protocol AnyMigration {
     static var name: String { get }
     static var migrationName: String { get }
     
-//    static func prepare(on conn: BridgeConnection) -> EventLoopFuture<Void>
-//    static func revert(on conn: BridgeConnection) -> EventLoopFuture<Void>
+    static func prepare(on conn: BridgeConnection) -> EventLoopFuture<Void>
+    static func revert(on conn: BridgeConnection) -> EventLoopFuture<Void>
     
     static func prepare(on conn: BridgeConnection) async throws
     static func revert(on conn: BridgeConnection) async throws
@@ -21,4 +21,11 @@ public protocol AnyMigration {
 extension AnyMigration {
     public static var name: String { String(describing: Self.self) }
     public static var migrationName: String { name }
+    
+    static func prepare(on conn: BridgeConnection) -> EventLoopFuture<Void> {
+        conn.eventLoop.future()
+    }
+    static func revert(on conn: BridgeConnection) -> EventLoopFuture<Void> {
+        conn.eventLoop.future()
+    }
 }
